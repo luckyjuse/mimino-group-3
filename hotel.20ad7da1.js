4b2ebc484f60805dde11e9af915d6ef3860f560a
luckyjuse <107860445+luckyjuse@users.noreply.github.com>
2022-12-14 10:31:31 +0000

    Deploying to gh-pages from @ luckyjuse/mimino-group-3@baee3e573ae5fcbaf68beb40f8da130c7afae2df 🚀

---
+++ hotel.20ad7da1.js
@@ -0,0 +1,2 @@
+$(".slider-2pictures").slick({mobileFirst:!0,slidesToShow:2,adaptiveHeight:!0});
+//# sourceMappingURL=hotel.20ad7da1.js.map
---
+++ hotel.491bd444.js
@@ -0,0 +1,2 @@
+$(".slider-2pictures").slick({mobileFirst:!0,slidesToShow:2,adaptiveHeight:!0});
+//# sourceMappingURL=hotel.491bd444.js.map
