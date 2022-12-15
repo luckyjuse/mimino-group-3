6dbe753a2898793a25e75a5e74fdcc9d1dfaba8c
luckyjuse <107860445+luckyjuse@users.noreply.github.com>
2022-12-15 12:05:48 +0000

    Deploying to gh-pages from @ luckyjuse/mimino-group-3@639b6da05c0c582169f3f5ccfb3ebf4c791cb68e 🚀

--- hotel.88f75e08.js
+++
@@ -1,2 +0,0 @@
-$(".slider-2pictures").slick({mobileFirst:!0,slidesToShow:2,adaptiveHeight:!0}),$(".slider-2pictures-invert").slick({mobileFirst:!0,slidesToShow:2,adaptiveHeight:!0});
-//# sourceMappingURL=hotel.88f75e08.js.map
--- hotel.b048fb32.js
+++
@@ -1,2 +0,0 @@
-$(".slider-2pictures").slick({mobileFirst:!0,slidesToShow:2,adaptiveHeight:!0}),$(".slider-2pictures-invert").slick({mobileFirst:!0,slidesToShow:2,adaptiveHeight:!0});
-//# sourceMappingURL=hotel.b048fb32.js.map
